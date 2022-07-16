7856ecc97a8aafbc09711e3d5e7127c2614ef02b
Aiga <aiga.andrejeva@gmail.com>
2022-07-16 11:31:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# main-javascript-project
+ Rock, paper and scissors game - part of Brainnest course.
---
+++ script.js
@@ -0,0 +1,75 @@
+//Computer randomly slecets rock, paper or scissors
+function computerPlay(){
+    const computerChoice = Math.floor(Math.random() * 3);
+    if (computerChoice == 0) {
+        return "rock";
+    } else if (computerChoice == 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    } 
+}
+
+//This function checks either user or computer wins
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === computerSelection) {
+        return "It's a tie";
+    } else if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            computerWin++;
+            return "Computer has won! Current score: " + computerWin;
+        } else {
+            userWin++;
+            return "You have won! Current score: " + userWin;
+            }    
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "scissors") {
+            computerWin++;
+            return "Computer has won! Current score: " + computerWin;        
+        } else {
+            userWin++;
+            return "You have won! Current score: " + userWin;
+            }
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+            computerWin++;
+            return "Computer has won! Current score: " + computerWin;
+        } else {
+            userWin++;
+            return "You have won! Current score: " + userWin;
+        }
+    } 
+}
+
+let computerWin = 0; 
+let userWin = 0;
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+
+function winnerIs(){
+    if (userWin > computerWin){
+        return "User has won with total score: " + userWin;
+    } else if (userWin < computerWin){
+        return "Computer has won with total score: " + computerWin;
+    } else {
+        return "It's a draw!"
+    }
+}
+
+//main game function
+function game(){
+    let rounds;
+    for (let i = 0; i < 5; i++){
+        rounds = (i + 1);
+        console.log("Round: " + rounds); 
+        let playerInput = window.prompt("Choose rock, paper or scissors");
+        let computerInput = computerPlay();
+        console.log("User chose: " + playerInput);
+        console.log("Computer chose: " + computerInput);
+        console.log(playRound(playerInput.toLowerCase(), computerInput));
+    }
+    console.log("The winner is: " + winnerIs());
+}
+
+//Calling the game function
+game();
