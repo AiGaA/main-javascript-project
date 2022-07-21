fb5c419f4c37558edb58a05413ad2d3c46e60f55
Aiga <aiga.andrejeva@gmail.com>
2022-07-21 20:59:08 +0100

    Added UI and eventListener to the game
    Added buttons, CSS style sheet and functionality for UI

--- script.js
+++ script.js
@@ -1,84 +1,71 @@
-//Computer randomly slecets rock, paper or scissors
+const selectionButtons = document.querySelectorAll('button')
+const userChoiceDisplay = document.getElementById('user-choice')
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const computerResultDisplay = document.getElementById('computer-score')
+const userResultDisplay = document.getElementById('user-score')
+const roundsDisplay = document.getElementById('play-round')
+let userChoice
+let computerChoice
+let computerWin = 0; 
+let userWin = 0;
+let rounds = 0;
+
+selectionButtons.forEach(possibleChoice => {
+    possibleChoice.addEventListener('click', e => {
+        userChoice = e.target.id
+        userChoiceDisplay.innerHTML = userChoice
+        computerPlay()
+        computerChoiceDisplay.innerHTML = computerChoice
+        playRound(userChoice, computerChoice)
+        roundsDisplay.innerHTML = rounds
+    })
+})
+
+//Computer randomly selecets rock, paper or scissors
 function computerPlay(){
-    const computerChoice = Math.floor(Math.random() * 3);
-    if (computerChoice == 0) {
-        return "rock";
-    } else if (computerChoice == 1) {
-        return "paper";
+    const randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber == 0) {
+        computerChoice = "rock";
+    } else if (randomNumber == 1) {
+        computerChoice = "paper";
     } else {
-        return "scissors";
-    } 
+        computerChoice = "scissors";
+    }
 }
 
 //This function checks either user or computer wins
 function playRound(playerSelection, computerSelection){
     if (playerSelection === computerSelection) {
-        return "It's a tie, try again!";
+        rounds--;
     } else if (playerSelection === "rock") {
         if (computerSelection === "paper") {
             computerWin++;
-            return "Computer has won! Current score: " + computerWin;
         } else {
             userWin++;
-            return "You have won! Current score: " + userWin;
             }    
     } else if (playerSelection === "paper") {
         if (computerSelection === "scissors") {
             computerWin++;
-            return "Computer has won! Current score: " + computerWin;        
         } else {
             userWin++;
-            return "You have won! Current score: " + userWin;
             }
     } else if (playerSelection === "scissors") {
         if (computerSelection === "rock") {
             computerWin++;
-            return "Computer has won! Current score: " + computerWin;
         } else {
             userWin++;
-            return "You have won! Current score: " + userWin;
         }
-    } else {
-        alert("Invalid input! Please enter rock, paper or scissors.");
-        rounds += 1;
-        return "Please try again.";
     }
+    rounds++;
+    computerResultDisplay.innerHTML = computerWin
+    userResultDisplay.innerHTML = userWin
+    winnerIs()
 }
-
-let computerWin = 0; 
-let userWin = 0;
-let rounds = 0;
 
 function winnerIs(){
-    if (userWin > computerWin){
-        return "User has won with total score: " + userWin;
-    } else if (userWin < computerWin){
-        return "Computer has won with total score: " + computerWin;
-    } else {
-        return "It's a draw!";
-    }
+    if (userWin == 5 ){
+        window.alert("Congratulations! User has won with total score: " + userWin);
+    } else if (computerWin == 5){
+        window.alert("Congratulations! Computer has won with total score: " + computerWin);
+    } 
 }
-
-//main game function
-function game(){
-    for (let i = 0; i < 5; i++){
-        rounds = (i + 1);
-        console.log("Round: " + rounds);
-        const playerInput = window.prompt("Choose rock, paper or scissors").toLowerCase();
-        if (playerInput === null) {
-            console.log("You've quit the game!");
-            return playerInput;
-        }
-        const computerInput = computerPlay();
-        console.log("User chose: " + playerInput);
-        console.log("Computer chose: " + computerInput);
-        console.log(playRound(playerInput, computerInput));
-        if (playerInput === computerInput){
-            i--;
-        }
-    }
-    console.log("The winner is: " + winnerIs());
-}
-
-//Calling the game function
-game();
